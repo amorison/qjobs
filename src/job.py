a7ce249e7e240f0c65c9a47e42388b5df633c46e
amorison <amorison@users.noreply.github.com>
2015-07-13 22:57:36 +0200

    docstrings of job module

--- src/job.py
+++ src/job.py
@@ -15,56 +15,69 @@ class Job:
             raise ValueError("dct must contains a 'i' key")
         self.idt = dct['i']
 
+
     def __hash__(self):
         """hash based on job id"""
         return hash(self.idt)
+
 
     def __eq__(self, other):
         """comparison based on job id"""
         return isinstance(other, self.__class__) and self.idt == other.idt
 
+
     def __ne__(self, other):
         """comparison based on job id"""
         return not self.__eq__(other)
-    
+
+
     def get(self, itm):
         """get job propriety"""
         return self.dct[itm]
+
 
     def rep(self, fmt):
         """representation of job based on format fmt"""
         return fmt.format(**self.dct)
 
+
 class JobList:
-    def __init__(self):
-        self.jobset = set()
+    """JobList class which handle the width of the different
+    fields and the job counting for total"""
+
+    def __init__(self, job_list):
+        """constructor expect a list of Job"""
+        self.jobset = set(job_list)
         self.width = {}
+        self.total = {}
         for itm in constants.itms:
-            self.width[itm] = 0
-            # sorted list of length (will make updates easier)
-            # should also be able to init with an actual list of jobs
-            # to avoid making a lot of "heavy" updates just to
-            # init the JobList...
+            self.width[itm] = sorted(len(job.get(itm))
+                                     for job in self.jobset)
+        for job in self.jobset:
+            for itm in constants.itms:
+
 
     def add(self, job):
-        # should remove then add
-        # and update width/total datas
-        self.jobset.add(job)
-
-    def replace(self, job):
+        """add a job, update width and total, and erase the previous
+        existing one with the same id if needed"""
         self.jobset.discard(job)
         self.jobset.add(job)
 
+
     def rep(self, fmt):
+        """handles the representation of the entire list"""
         # will have to work a bit on the format to put the width of the fields
         # will have to sort jobset
         # replace output.out
         for job in self.jobset:
             print(job.rep(fmt))
 
-    # need a function to replace output.total
-    # total will need to be updated as the width
-    # misc.get_itms: will construct the groups and JobLists? (will be renamed)
+
+    def rep_tot(self, tot_list, fmt):
+        """handles the representation of the totals"""
+        # will need args.total -> tot_list
+        pass
+
 
 class JobGroup:
     pass

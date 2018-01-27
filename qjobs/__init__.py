bd27cfda91f7eb4bbdac7c0b3ede98962aeb9cc6
Adrien Morison <adrien.morison@gmail.com>
2018-01-27 18:54:37 +0100

    Fix __version__ gathering

--- qjobs/__init__.py
+++ qjobs/__init__.py
@@ -9,6 +9,6 @@ from pkg_resources import get_distribution, DistributionNotFound
 try:
     __version__ = get_version(root='..', relative_to=__file__)
 except LookupError:
-    __version__ = get_distribution('stagpy').version
+    __version__ = get_distribution('qjobs').version
 except (DistributionNotFound, ValueError):
     __version__ = 'unknown'
